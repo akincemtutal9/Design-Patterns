92d0635ec98d88ad154342d58b027b9aeb5995bd
akincemtutal9 <akintutall@gmail.com>
2022-03-25 15:51:29 +0300

    Jewelery Material
    In this project this material used for calculating price of jewellery

---
+++ Materials.java
@@ -0,0 +1,10 @@
+public interface Materials {
+  /**
+    * Money constants for given materials.
+    * These will be called in main method while creating price.
+    * 
+    */ 
+    double GOLD = 10.0;
+    double SILVER = 6.0;
+    double DIAMOND = 12.0;
+    }
